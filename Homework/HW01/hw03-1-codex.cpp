7c91d3febb0446f3d5f78891644cdb37f9e7dc94
XxP228 <kamochnok.2548@gmail.com>
2023-08-14 21:47:03 +0700

    hw3-1111111111111111

--- Homework/HW01/hw03-1-codex.cpp
+++ Homework/HW01/hw03-1-codex.cpp
@@ -1,6 +1,5 @@
 // TODO: #8 Arrange this C coding into CODEX pattern.
 #include <stdio.h>
-#include <conio.h>
 int main()
 {
   int a = 10 ;
@@ -10,7 +9,7 @@ int main()
   {
     if ( i < 30 )
     {
-      printf( " Here" ) ;
+      printf( "Here " ) ;
       if ( i == 8 )
       {
         printf( " This is 8." ) ;
